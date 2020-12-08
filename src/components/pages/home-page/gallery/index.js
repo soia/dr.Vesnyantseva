29cc08c23afc457377941b1b249316cec28636e0
Nikita Soia <nikita.soya@gmail.com>
2020-12-08 23:35:27 +0200

    added education block

--- src/components/pages/home-page/gallery/index.js
+++ src/components/pages/home-page/gallery/index.js
@@ -1,17 +1,147 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Coverflow from 'react-coverflow';
+import img1 from './images/1.jpeg';
+import img2 from './images/2.JPG';
+import img3 from './images/3.JPG';
+import img4 from './images/4.JPG';
+import img5 from './images/5.JPG';
+import img6 from './images/6.JPG';
+import img7 from './images/7.JPG';
+import img8 from './images/8.JPG';
+import img9 from './images/9.JPG';
+import img10 from './images/10.JPG';
+import img11 from './images/11.JPG';
+import img12 from './images/12.JPG';
+import img13 from './images/13.JPG';
+import img14 from './images/14.JPG';
+import img15 from './images/15.jpeg';
+import img16 from './images/16.jpeg';
+import img17 from './images/17.jpeg';
+import img18 from './images/18.jpeg';
+import img19 from './images/19.jpeg';
+import img20 from './images/20.jpeg';
+import img21 from './images/21.jpeg';
 import style from './style.module.scss';
 import './background.scss';
 
 const Gallery = () => {
     const { t } = useTranslation();
 
+    const gallery = [
+        {
+            img: img1,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img2,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img3,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img4,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img5,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img6,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img7,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img8,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img9,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img10,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img11,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img12,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img13,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img14,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img15,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img16,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img17,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img18,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img19,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img20,
+            title: 'Какой-то текст',
+        },
+        {
+            img: img21,
+            title: 'Какой-то текст',
+        },
+    ];
+
     return (
         <div className={style.container} id="gallery">
             <div className={style.wrapper}>
                 <h3 className={style.wrapper__title}>{t('gallery')}</h3>
             </div>
+            <div className={style.slider}>
+                <Coverflow
+                    displayQuantityOfSide={2}
+                    navigation={false}
+                    enableScroll={false}
+                    clickable
+                    infiniteScroll
+                    active={0}
+                >
+                    {gallery.map(item => {
+                        const { img, title } = item;
 
+                        return (
+                            <img
+                                key={img}
+                                src={img}
+                                alt={title}
+                            />
+                        );
+                    })}
+                </Coverflow>
+            </div>
             <link
                 href="https://fonts.googleapis.com/css?family=Lato:300,400,700"
                 rel="stylesheet"
