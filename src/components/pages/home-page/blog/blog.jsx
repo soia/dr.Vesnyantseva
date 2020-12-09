26a97f4c0345656130216914ce731da09e0fa97b
Nikita Soia <nikita.soya@gmail.com>
2020-12-09 18:51:24 +0200

    blog

--- src/components/pages/home-page/blog/blog.jsx
+++ src/components/pages/home-page/blog/blog.jsx
@@ -1,16 +1,33 @@
-/* eslint-disable camelcase */
-import React, { Fragment, useEffect, useState } from 'react';
-import LinesEllipsis from 'react-lines-ellipsis';
-import { useTranslation } from 'react-i18next';
-import Moment from 'react-moment';
-import avatar from '../../../assets/images/portrets/portret.png';
+import React, { Component } from 'react';
+import { withTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+import { compose } from '../../../../utils';
+import Posts from './list';
 import style from './blog.module.scss';
 
-const Blog = () => {
-    const [insta, setInsta] = useState([]);
-    const { t } = useTranslation();
+const postsPerPage = 3;
+let arrayForHoldingPosts = [];
 
-    const loadData = async () => {
+class Blog extends Component {
+    static defaultProps = {
+        t: () => {},
+    };
+
+    static propTypes = {
+        t: PropTypes.func,
+    };
+
+    state = {
+        insta: [],
+        postsToShow: [],
+        next: 3,
+    };
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    loadData = async () => {
         // const response = await fetch(
         //     'https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=IGQVJWbmVFTGdSYkRCajU2bk93dHIzTHRiUjloQlJaQUJtaHBvVlZAIU2dGR0pFdG5TWkFiU01GWGxwbG9iSmFKc0VoLWhfdGxfWjRYdlhOcHJ1MHRFdUZAHRkp6VUhpekNwdy1UcG8taUl2dmNYcnhkRAZDZD',
         // );
@@ -19,72 +36,54 @@ const Blog = () => {
         );
 
         const user = await response.json();
-
         const filterOnlyImage = user.data.filter(item => item.media_type === 'IMAGE');
-        setInsta(filterOnlyImage);
+
+        this.setState({
+            insta: filterOnlyImage,
+        }, () => {
+            this.loopWithSlice(0, postsPerPage);
+        });
     };
 
-    useEffect(() => {
-        loadData();
-    }, []);
+    loopWithSlice = (start, end) => {
+        const { insta } = this.state;
+        const slicedPosts = insta.slice(start, end);
+        arrayForHoldingPosts = [...arrayForHoldingPosts, ...slicedPosts];
+        this.setState({
+            postsToShow: arrayForHoldingPosts,
+        });
+    };
 
-    return (
-        <Fragment>
+    handleShowMorePosts = () => {
+        const { next } = this.state;
+
+        this.loopWithSlice(next, next + postsPerPage);
+        this.setState({
+            next: next + postsPerPage,
+        });
+    };
+
+    render() {
+        const { t } = this.props;
+        const { insta, postsToShow } = this.state;
+
+        return (
             <div className={style.blog} id="blog">
-                <h3 className={style.blog__title}>{t('blog')}</h3>
-                <div className={style.insta}>
-                    {insta.map(item => {
-                        const {
-                            caption,
-                            id,
-                            media_url,
-                            permalink,
-                            timestamp,
-                            username,
-                        } = item;
-
-                        return (
-                            <a
-                                href={permalink}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                key={id}
-                                className={style.insta__item}
-                            >
-                                <div className={style.insta__top}>
-                                    <img
-                                        className={style.insta__top_pic}
-                                        src={avatar}
-                                        alt="pic"
-                                    />
-                                    <p className={style.insta__top_username}>
-                                        {username}
-                                    </p>
-                                </div>
-                                <img
-                                    className={style.insta__item_img}
-                                    src={media_url}
-                                    alt="url"
-                                />
-                                <p className={style.insta__item_time}>
-                                    <Moment format="DD/MM/YYYY">{timestamp}</Moment>
-                                </p>
-                                <div className={style.insta__item_caption}>
-                                    <LinesEllipsis
-                                        text={caption}
-                                        maxLine="3"
-                                        ellipsis="..."
-                                        trimRight
-                                        basedOn="letters"
-                                    />
-                                </div>
-                            </a>
-                        );
-                    })}
+                <h3 className={style.blog__title}>
+                    {t('blog')}
+                </h3>
+                <div>
+                    <Posts postsToRender={postsToShow} insta={insta} />
+                    <button
+                        className={style.blog__loadMore}
+                        onClick={this.handleShowMorePosts}
+                    >
+                        {t(t('loadMore'))}
+                    </button>
                 </div>
             </div>
-        </Fragment>
-    );
-};
+        );
+    }
+}
 
-export default Blog;
+export default compose(withTranslation())(Blog);
---
+++ src/components/pages/home-page/blog/list.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable camelcase */
+import React from 'react';
+import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis';
+import Moment from 'react-moment';
+import avatar from '../../../assets/images/portrets/portret.png';
+import style from './blog.module.scss';
+
+const Posts = ({ postsToRender }) => (
+    <div className={style.insta}>
+        {postsToRender.map(item => {
+            const {
+                caption, id, media_url, permalink, timestamp, username,
+            } = item;
+
+            return (
+                <a
+                    href={permalink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    key={id}
+                    className={style.insta__item}
+                >
+                    <div className={style.insta__top}>
+                        <img className={style.insta__top_pic} src={avatar} alt="pic" />
+                        <p className={style.insta__top_username}>{username}</p>
+                    </div>
+                    <img className={style.insta__item_img} src={media_url} alt="url" />
+                    <p className={style.insta__item_time}>
+                        <Moment format="DD/MM/YYYY">{timestamp}</Moment>
+                    </p>
+                    <div className={style.insta__item_caption}>
+                        <LinesEllipsis
+                            text={caption}
+                            maxLine="3"
+                            ellipsis="..."
+                            trimRight
+                            basedOn="letters"
+                        />
+                    </div>
+                </a>
+            );
+        })}
+    </div>
+);
+
+Posts.defaultProps = {
+    postsToRender: [],
+};
+
+Posts.propTypes = {
+    postsToRender: PropTypes.instanceOf(Array),
+};
+
+export default Posts;
