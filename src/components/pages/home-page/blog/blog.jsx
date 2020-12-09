216f7f563130d2654ec30fb50b64ddcd58d4b662
Nikita Soia <nikita.soya@gmail.com>
2020-12-09 12:27:53 +0200

    added education block

--- src/components/pages/home-page/blog/blog.jsx
+++ src/components/pages/home-page/blog/blog.jsx
@@ -1,60 +1,70 @@
-/* eslint-disable react/no-array-index-key */
-import React, { Fragment } from 'react';
-import { useTrail, animated } from 'react-spring';
+/* eslint-disable camelcase */
+import React, { Fragment, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import PropTypes from 'prop-types';
 import style from './blog.module.scss';
 
-const fast = { tension: 1200, friction: 40 };
-const slow = { mass: 10, tension: 200, friction: 50 };
-const trans = (x, y) => `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`;
-
 const Blog = () => {
+    const [insta, setInsta] = useState({});
     const { t } = useTranslation();
-    const [trail, set] = useTrail(3, () => ({
-        xy: [0, 0],
-        config: i => (i === 0 ? fast : slow),
-    }));
+
+    const loadData = async () => {
+        const response = await fetch(
+            'https://www.instagram.com/dr.vesnyantseva/?__a=1&max_id=100',
+        );
+
+        const user = await response.json();
+        setInsta(user.graphql.user);
+    };
+
+    useEffect(() => {
+        loadData();
+    }, []);
+
+    const viewsData = insta?.edge_owner_to_timeline_media?.edges || [];
 
     return (
         <Fragment>
             <div className={style.blog} id="blog">
                 <h3 className={style.blog__title}>{t('blog')}</h3>
-                <svg style={{ position: 'absolute', width: 0, height: 0 }}>
-                    <filter id="goo">
-                        <feGaussianBlur
-                            in="SourceGraphic"
-                            result="blur"
-                            stdDeviation="30"
-                        />
-                        <feColorMatrix
-                            in="blur"
-                            values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7"
-                        />
-                    </filter>
-                </svg>
-                <div
-                    className="hooks-main"
-                    onMouseMove={e => set({ xy: [e.clientX, e.clientY] })}
-                >
-                    {trail.map((props, index) => (
-                        <animated.div
-                            key={index}
-                            style={{ transform: props.xy.interpolate(trans) }}
-                        />
-                    ))}
+                <div className={style.insta}>
+                    {viewsData.map(item => {
+                        const {
+                            display_url,
+                            edge_liked_by: { count },
+                            location,
+                            taken_at_timestamp,
+                        } = item.node;
+
+                        console.log(insta, 'instainstainsta');
+                        return (
+                            <div className={style.insta__item}>
+                                <div className={style.insta__top}>
+                                    <img
+                                        className={style.insta__top_pic}
+                                        src={insta.profile_pic_url_hd}
+                                        alt="pic"
+                                    />
+                                    <div>
+                                        <p className={style.insta__top_username}>
+                                            {insta.username}
+                                        </p>
+                                        <p className={style.insta__top_location}>
+                                            {location && location.name}
+                                        </p>
+                                    </div>
+                                </div>
+                                <img
+                                    className={style.insta__item_img}
+                                    src={display_url}
+                                    alt="url"
+                                />
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </Fragment>
     );
-};
-
-Blog.defaultProps = {
-    xy: {},
-};
-
-Blog.propTypes = {
-    xy: PropTypes.object,
 };
 
 export default Blog;
