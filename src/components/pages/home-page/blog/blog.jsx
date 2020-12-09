7552cc568e4b2a34f30a99a05978202d5d922467
Nikita Soia <nikita.soya@gmail.com>
2020-12-09 19:51:34 +0200

    blog

--- src/components/pages/home-page/blog/blog.jsx
+++ src/components/pages/home-page/blog/blog.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { compose } from '../../../../utils';
 import Posts from './list';
 import style from './blog.module.scss';
 
-const postsPerPage = 3;
+const postsPerPage = 6;
 let arrayForHoldingPosts = [];
 
 class Blog extends Component {
@@ -20,11 +21,24 @@ class Blog extends Component {
     state = {
         insta: [],
         postsToShow: [],
-        next: 3,
+        next: 6,
+        isShowHide: false,
     };
 
     componentDidMount() {
         this.loadData();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const { insta, postsToShow } = this.state;
+
+        if (postsToShow.length !== prevState.postsToShow.length) {
+            if (insta.length === postsToShow.length) {
+                this.setState({
+                    isShowHide: true,
+                });
+            }
+        }
     }
 
     loadData = async () => {
@@ -38,11 +52,14 @@ class Blog extends Component {
         const user = await response.json();
         const filterOnlyImage = user.data.filter(item => item.media_type === 'IMAGE');
 
-        this.setState({
-            insta: filterOnlyImage,
-        }, () => {
-            this.loopWithSlice(0, postsPerPage);
-        });
+        this.setState(
+            {
+                insta: filterOnlyImage,
+            },
+            () => {
+                this.loopWithSlice(0, postsPerPage);
+            },
+        );
     };
 
     loopWithSlice = (start, end) => {
@@ -63,23 +80,40 @@ class Blog extends Component {
         });
     };
 
+    close = () => {
+        arrayForHoldingPosts = [];
+        this.loopWithSlice(0, postsPerPage);
+        this.setState({
+            next: 6,
+            isShowHide: false,
+        });
+    };
+
     render() {
         const { t } = this.props;
-        const { insta, postsToShow } = this.state;
+        const { insta, postsToShow, isShowHide } = this.state;
 
         return (
             <div className={style.blog} id="blog">
-                <h3 className={style.blog__title}>
-                    {t('blog')}
-                </h3>
+                <h3 className={style.blog__title}>{t('blog')}</h3>
                 <div>
                     <Posts postsToRender={postsToShow} insta={insta} />
-                    <button
-                        className={style.blog__loadMore}
-                        onClick={this.handleShowMorePosts}
-                    >
-                        {t(t('loadMore'))}
-                    </button>
+                    {isShowHide ? (
+                        <AnchorLink
+                            onClick={this.close}
+                            href="#blog"
+                            className={style.blog__loadMore}
+                        >
+                            {t(t('hide'))}
+                        </AnchorLink>
+                    ) : (
+                        <button
+                            className={style.blog__loadMore}
+                            onClick={this.handleShowMorePosts}
+                        >
+                            {t('loadMore')}
+                        </button>
+                    )}
                 </div>
             </div>
         );
