9d426c892cf882bb771ea5e6200fade1b2cb454c
Nikita Soia <nikita.soya@gmail.com>
2020-12-11 16:05:17 +0200

    box page

--- src/components/pages/home-page/boxes/boxes.jsx
+++ src/components/pages/home-page/boxes/boxes.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useSpring, animated } from 'react-spring';
+import setIcon from './images/setIcon.png';
 import style from './boxes.module.scss';
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
 const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`;
-const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`;
+const trans2 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`;
 const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`;
-const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`;
-const trans5 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 4 - 180}px,0)`;
+const trans4 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`;
+const trans5 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`;
 
 const Boxes = () => {
     const { t } = useTranslation();
@@ -22,9 +24,13 @@ const Boxes = () => {
     return (
         <div className={style.boxes} id="boxes">
             <div className={style.boxes__leftSide}>
-                <h3 className={style.boxes__title}>
-                    {t('boxes')}
-                </h3>
+                <h3 className={style.boxes__title}>{t('healthBox')}</h3>
+                <p className={style.boxes__subTitle}>{t('giveOralCare')}</p>
+                <img className={style.boxes__leftSide_img} src={setIcon} alt="setIcon" />
+                <div className={style.boxes__buttonWr}>
+                    <Link to="/">{t('moreDetails')}</Link>
+                    <button type="button">{t('buy')}</button>
+                </div>
             </div>
             <div
                 className={style.container}
@@ -52,7 +58,6 @@ const Boxes = () => {
                 />
             </div>
         </div>
-
     );
 };
 
